84bfe6f48353b93e42e66f082759081c8810d657
Meile5 <meile.kapisaite5@gmail.com>
2026-02-22 18:48:31 +0100

    database migrations

---
+++ ProfanityService/Migrations/20260222174819_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProfanityService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    WordId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Word = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Words__2C20F066E3356C38", x => x.WordId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Words");
+        }
+    }
+}
