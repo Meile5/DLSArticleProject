684ea84973c1c890536548af785e8ef4e2925c73
Meile5 <meile.kapisaite5@gmail.com>
2026-03-12 08:29:19 +0100

    publisher service

---
+++ PublisherService/Entities/PublishArticleRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace PublisherService.Entities;
+
+public class PublishArticleRequest
+{
+    public required string Title { get; set; }
+    public required string Content { get; set; }
+    public required Guid AuthorId { get; set; }
+}
---
+++ PublisherService/Services/PublisherService.cs
@@ -0,0 +1,20 @@
+﻿using ArticleQueue.Interfaces;
+using ArticleQueue.Models.Events;
+using PublisherService.Entities;
+
+namespace PublisherService.Services;
+
+public class PublisherService(IMessageClient _client)
+{
+    public async Task PublishArticleAsync(PublishArticleRequest request)
+    {
+        await _client.Publish(new ArticlePublishedEvent
+        {
+            ArticleId = Guid.NewGuid(),
+            Title = request.Title,
+            Content = request.Content,
+            AuthorId = request.AuthorId,
+            PublishedAt = DateTime.UtcNow
+        });
+    }
+}
