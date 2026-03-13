34f8858e777084ddb1da546234c7a3087c3f60e3
Nelson Veríssimo <nelson.sverissimo@gmail.com>
2026-03-13 22:56:30 +0100

    Added dashboard showing cache hit ratios (missing on article cache)

--- .idea/.idea.ArticleProject/.idea/queries/Query_1.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE profanity-db-real;
-use profanity-db-real;
-
-CREATE TABLE Words(
-                      WordId nvarchar(50) NOT NULL PRIMARY KEY,
-                      Word nvarchar(50) NOT NULL,
-
-);
